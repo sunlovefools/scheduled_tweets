11653eca3a1b4c8065ada03db1e4c946b9bc8260
Ng Yoong Shen <ngyoonshen331@gmail.com>
2024-08-05 10:42:15 +0800

    Add Flash Message

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
     def main
+        flash.now[:notice] = "Successfully logged in"
+        flash.now[:alert] = "Unsuccessful login"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,4 @@ Rails.application.routes.draw do
   get 'test', to: 'about#hello'
   root to:"main#main"
 end
+  
